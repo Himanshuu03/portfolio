7ffa7270c13b821cca4c521e17c786054872449b
Himanshuu03 <himanshujangra2403@gmail.com>
2024-02-24 19:04:25 +0530

    portofolio

---
+++ src/components/projectData.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    title: "Project 1",
+    img: "http:img/source",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum consequatur architecto deleniti dolor deserunt rem minima blanditiis explicabo. Ipsam voluptatibus facilis eveniet excepturi. Fugit officiis accusantium, optio delectus unde fuga.",
+    githubLink :"https://github.com/Himanshuu03/dataStructure",
+    hostLink :"http:img/source"  
+  },
+  {
+    title: "Project 1",
+    img: "http:img/source",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum consequatur architecto deleniti dolor deserunt rem minima blanditiis explicabo. Ipsam voluptatibus facilis eveniet excepturi. Fugit officiis accusantium, optio delectus unde fuga.",
+    githubLink :"http:img/source",
+    hostLink :"http:img/source"  
+  },
+  {
+    title: "Project 1",
+    img: "http:img/source",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum consequatur architecto deleniti dolor deserunt rem minima blanditiis explicabo. Ipsam voluptatibus facilis eveniet excepturi. Fugit officiis accusantium, optio delectus unde fuga.",
+    githubLink :"http:img/source",
+    hostLink :"http:img/source"  
+  },
+  {
+    title: "Project 1",
+    img: "http:img/source",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum consequatur architecto deleniti dolor deserunt rem minima blanditiis explicabo. Ipsam voluptatibus facilis eveniet excepturi. Fugit officiis accusantium, optio delectus unde fuga.",
+    githubLink :"http:img/source",
+    hostLink :"http:img/source"  
+  },
+];
